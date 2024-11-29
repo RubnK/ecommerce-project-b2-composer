6086dfeae70fdeff1e76b7790d1fb7c2656aafa5
RubnK <rubenkalifa@gmail.com>
2024-11-29 10:09:27 +0100

    Ajout de la page panier

---
+++ src/views/panier.php
@@ -0,0 +1,27 @@
+<h1>Votre Panier</h1>
+<div class="cart">
+    <div class="item">
+        <img src="/imgs/SoCode.png" alt="SoCode">
+        <div class="details">
+            <h2>SoCode</h2>
+            <p>Prix: 9,99€</p>
+            <label for="quantity1">Quantité:</label>
+            <input type="number" id="quantity1" name="quantity1" value="1" min="1">
+            <button class="remove">Supprimer</button>
+        </div>
+    </div>
+    <div class="item">
+        <img src="/imgs/SoManage.png" alt="SoManage">
+        <div class="details">
+            <h2>SoManage</h2>
+            <p>Prix: 14,99€</p>
+            <label for="quantity2">Quantité:</label>
+            <input type="number" id="quantity2" name="quantity2" value="1" min="1">
+            <button class="remove">Supprimer</button>
+        </div>
+    </div>
+</div>
+<div class="total">
+    <h2>Total: 24,98€</h2>
+    <a class="checkout" href="/">Commander</a>
+</div>
--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -9,7 +9,16 @@
     <link rel="stylesheet" href="/css/home.css">
     <?php } elseif ($_SERVER['REQUEST_URI'] == '/catalogue'){ ?>
     <link rel="stylesheet" href="/css/catalogue.css">
+    <?php } elseif ($_SERVER['REQUEST_URI'] == '/panier'){ ?>
+    <link rel="stylesheet" href="/css/panier.css">
+    <?php } elseif ($_SERVER['REQUEST_URI'] == '/login'){ ?>
+    <link rel="stylesheet" href="/css/login.css">
+    <?php } elseif ($_SERVER['REQUEST_URI'] == '/signup'){ ?>
+    <link rel="stylesheet" href="/css/signup.css">
+    <?php } elseif ($_SERVER['REQUEST_URI'] == '/SoCode' || $_SERVER['REQUEST_URI'] == '/SoManage' || $_SERVER['REQUEST_URI'] == '/SoDraw' || $_SERVER['REQUEST_URI'] == '/SoNote'){ ?>
+    <link rel="stylesheet" href="/css/produit.css">
     <?php } ?>
+
 </head>
 <body>
     <nav>
